3dc60a3ee7bbfbb7b2f45ba123ba298dd6709132
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-01 15:26:31 +0100

    Corretto

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 const bombs = 16;
 const first = 1;
+var last;
+var difficulty = prompt('scegliere livello di difficoltà 0/1/2');
+if (difficulty == '0') {
+    last = 100;
+}
+else if (difficulty == '1') {
+    last = 80;
+}
+else if (difficulty == '2') {
+    last = 20;
+}
 
 // function che genera n numeri random compresi tra min e max
 function nRandMinToMax(n,min,max) {
@@ -21,35 +32,35 @@ function nRandMinToMax(n,min,max) {
     return array;
 
 }
-var bombsArray = nRandMinToMax(bombs,first,100);
+var bombsArray = nRandMinToMax(bombs,first,last);
 console.log(bombsArray);
 
 var userNums = [];
 var bool1 = 'vinto';
-for (var i = 0; i < bombs; i++) {
-    userNums[i] = prompt('inserire numero n. ' + (i + 1) + ' ' + 'di ' + bombs);
-    if (bombsArray.indexOf(parseInt(userNums[i])) != -1) {
+for (var i = 0; i < last - bombs; i++) {
+    userNums[i] = prompt('inserire numero n. ' + (i + 1) + ' ' + 'di ' + (last - bombs));
+    if (bombsArray.indexOf(parseInt(userNums[i])) != -1 && parseInt(userNums[i]) == userNums[i] && userNums[i] >= first && userNums[i] <= last) {
         bool1 = 'perso';
-        var bool2 = prompt('hai perso, totalizzando ' + i + ' punti. vuoi ricominciare? si/no');
+        var bool2 = prompt('hai ' + bool1 + ', totalizzando ' + i + ' punti. vuoi ricominciare? si/no');
         break;
     }
-    while ((userNums.indexOf(userNums[i]) != -1 && userNums.indexOf(userNums[i]) < i) || parseInt(userNums[i]) != userNums[i] || userNums[i] < 1 || userNums[i] > 100) {
+    while ((userNums.indexOf(userNums[i]) != -1 && userNums.indexOf(userNums[i]) < i) || parseInt(userNums[i]) != userNums[i] || userNums[i] < first || userNums[i] > last) {
         alert('valore non valido');
-        userNums[i] = prompt('inserire numero n. ' + (i + 1) + ' ' + 'di ' + bombs);
+        userNums[i] = prompt('inserire numero n. ' + (i + 1) + ' ' + 'di ' + (last - bombs));
         if (bombsArray.indexOf(parseInt(userNums[i])) != -1) {
             bool1 = 'perso';
-            var bool2 = prompt('hai perso, totalizzando ' + i + ' punti. vuoi ricominciare? si/no');
+            var bool2 = prompt('hai ' + bool1 + ', totalizzando ' + i + ' punti. vuoi ricominciare? si/no');
             break;
         }
     }
 }
-if (bool1 = 'perso' && bool2 == 'si') {
+
+if (bool2 == 'si') {
     location.reload();
 }
-else if (bool1 = 'vinto') {
-    var bool2 = prompt('hai vinto! vuoi ricominciare? si/no');
-    if (bool2 == 'si') {
-        location.reload();
-    }
+
+if (bool1 == 'vinto') {
+    alert('hai ' + bool1 + '!');
 }
+
 console.log(userNums);
