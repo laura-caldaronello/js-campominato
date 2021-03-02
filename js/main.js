a33722891b5f9eb86a84186e20232d2b44c77e03
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-02 15:32:44 +0100

    Versione grafica molto semplice

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+// function che genera n numeri random compresi tra min e max
+function nRandMinToMax(n,min,max) {
+
+    var array = [];
+
+    if (max - min + 1 >= n) {
+        for (var i = 0; i < n; i++) {
+            array[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+            while (array.indexOf(array[i]) != -1 && array.indexOf(array[i]) < i) { /* per assicurarsi che siano tutti diversi */
+                array[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+        }
+    }
+    else if (max - min + 1 < n) {
+        alert('impossibile generare numeri diversi tra loro');
+    }
+
+    return array;
+
+}
+// /function
+
+// function isInArray
+function isInArray(value,array) {
+    var bool = false;
+    var i = 0;
+    while (i < array.length && bool == false) {
+        if (array[i] == value) {
+            bool = true;
+        }
+        i++;
+    }
+    return bool;
+}
+// /function
+
 // lascio commentate delle parti che possono essere la base per la traduzione grafica del gioco (se avrò tempo proverò a farlo)
 
 const bombs = 16;
@@ -23,90 +59,72 @@ switch (difficulty) {
         break;
 }
 
-// for (var i = 0; i < last; i++) {
-//     document.getElementById('table').innerHTML += '<div class="squares"></div>';
-// }
-
-// function che genera n numeri random compresi tra min e max
-function nRandMinToMax(n,min,max) {
-
-    var array = [];
-
-    if (max - min + 1 >= n) {
-        for (var i = 0; i < n; i++) {
-            array[i] = Math.floor(Math.random() * (max - min + 1)) + min;
-            while (array.indexOf(array[i]) != -1 && array.indexOf(array[i]) < i) { /* per assicurarsi che siano tutti diversi */
-                array[i] = Math.floor(Math.random() * (max - min + 1)) + min;
-            }
-        }
-    }
-    else {
-        alert('impossibile generare numeri diversi tra loro');
-    }
-
-    return array;
-
-}
 var bombsArray = nRandMinToMax(bombs,first,last);
 console.log(bombsArray);
 
-// function isInArray
-function isInArray(value,array) {
-    var bool = false;
-    var i = 0;
-    while (i < array.length && bool == false) {
-        if (array[i] == value) {
-            bool = true;
+// versione grafica
+
+// generazione di tutte le caselle cliccabili
+for (var i = 0; i < last; i++) {
+    document.getElementById('table').innerHTML += '<div class="squares"></div>';
+}
+
+// generazione bombe in caselle vietate
+var elements = document.getElementsByClassName('squares');
+var j = 0; /* per contare i click */
+
+for (let i = 0; i < last; i++) { /* approfondire perchè con var non funziona e con let si */
+    elements[i].addEventListener('click',function() {
+        if (isInArray(i + 1,bombsArray)) {
+            alert('hai perso, totalizzando ' + j + ' punti!');
+            location.reload();
         }
-        i++;
+        else {
+            elements[i].classList += ' white';
+            j++;
+            if (j == last - bombs) {
+                alert('hai vinto!');
+                location.reload();
+            }
+        }
     }
-    return bool;
+    );
 }
 
 
-// for (var i = 0; i < bombsArray.length; i++) {
-//     document.getElementsByClassName('squares')[bombsArray[i]].innerHTML += '<div class="bombs"></div>';
+// /versione grafica
+
+// // versione non grafica
+
+// var userNums = [];
+// var bool1 = 'vinto';
+// var i = 0;
+// while (i < last - bombs && bool1 == 'vinto') {
+//     do {
+//         userNums[i] = prompt('inserire numero n. ' + (i + 1) + ' ' + 'di ' + (last - bombs));
+//         if (isInArray(parseInt(userNums[i]),bombsArray)) {
+//             bool1 = 'perso';
+//             var userNums = []; /* in caso si volesse ricominciare */
+//             var bool2 = prompt('hai ' + bool1 + ', totalizzando ' + i + ' punti. vuoi ricominciare? si/no');
+//         }
+//         else if ((isInArray(parseInt(userNums[i]),userNums) && userNums.indexOf(userNums[i]) < i) || parseInt(userNums[i]) != userNums[i] || userNums[i] < first || userNums[i] > last) { /* è stato già inserito o è un carattere o valore non valido */
+//             alert('valore non valido');
+//         }
+//     } while (((isInArray(parseInt(userNums[i]),userNums) && userNums.indexOf(userNums[i]) < i) || parseInt(userNums[i]) != userNums[i] || userNums[i] < first || userNums[i] > last) && bool1 == 'vinto')
+//     i++;
 // }
 
-// var elements = document.getElementsByClassName('squares');
-// console.log(elements[0]);
-// elements[0].classList.add('grey');
-// console.log(elements[0]);
-
-// for (var i = 0; i < last; i++) {
-//     elements[i].addEventListener('click',function() {
-//         elements[i].classList.add('grey');
-//     }
-//     );
+// if (bool2 == 'si') {
+//     location.reload();
+// }
+// else if (bool2 != 'no' && bool2 != 'si' && bool1 == 'perso') {
+//     alert('non ho capito ma ok');
 // }
 
-var userNums = [];
-var bool1 = 'vinto';
-var i = 0;
-while (i < last - bombs && bool1 == 'vinto') {
-    do {
-        userNums[i] = prompt('inserire numero n. ' + (i + 1) + ' ' + 'di ' + (last - bombs));
-        if (isInArray(parseInt(userNums[i]),bombsArray)) {
-            bool1 = 'perso';
-            var userNums = []; /* in caso si volesse ricominciare */
-            var bool2 = prompt('hai ' + bool1 + ', totalizzando ' + i + ' punti. vuoi ricominciare? si/no');
-        }
-        else if ((isInArray(parseInt(userNums[i]),userNums) && userNums.indexOf(userNums[i]) < i) || parseInt(userNums[i]) != userNums[i] || userNums[i] < first || userNums[i] > last) { /* è stato già inserito o è un carattere o valore non valido */
-            alert('valore non valido');
-        }
-    } while (((isInArray(parseInt(userNums[i]),userNums) && userNums.indexOf(userNums[i]) < i) || parseInt(userNums[i]) != userNums[i] || userNums[i] < first || userNums[i] > last) && bool1 == 'vinto')
-    i++;
-}
+// if (bool1 == 'vinto') {
+//     alert('hai ' + bool1 + '!');
+// }
 
-if (bool2 == 'si') {
-    location.reload();
-}
-else if (bool2 != 'no' && bool2 != 'si' && bool1 == 'perso') {
-    alert('non ho capito ma ok');
-}
+// console.log(userNums);
 
-if (bool1 == 'vinto') {
-    alert('hai ' + bool1 + '!');
-}
-
-console.log(userNums);
+// // /versione non grafica
