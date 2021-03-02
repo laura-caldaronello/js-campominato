1c317ad90fd5597feaa299f3e6356aaad655a8ec
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-02 13:37:19 +0100

    Sistemato alcune cose dopo correzione

--- js/main.js
+++ js/main.js
@@ -8,14 +8,19 @@ var difficulty = prompt('scegliere livello di difficoltà 0/1/2');
 while (difficulty != '0' && difficulty != '1' && difficulty != '2') {
     difficulty = prompt('valore non valido. scegliere livello di difficoltà 0/1/2');
 }
-if (difficulty == '0') {
-    last = 100;
-}
-else if (difficulty == '1') {
-    last = 80;
-}
-else if (difficulty == '2') {
-    last = 50;
+
+switch (difficulty) {
+    case '0':
+        last = 100;
+        break;
+    case '1':
+        last = 80;
+        break;
+    case '2':
+        last = 50;
+        break;
+    default:
+        break;
 }
 
 // for (var i = 0; i < last; i++) {
@@ -45,6 +50,20 @@ function nRandMinToMax(n,min,max) {
 var bombsArray = nRandMinToMax(bombs,first,last);
 console.log(bombsArray);
 
+// function isInArray
+function isInArray(value,array) {
+    var bool = false;
+    var i = 0;
+    while (i < array.length && bool == false) {
+        if (array[i] == value) {
+            bool = true;
+        }
+        i++;
+    }
+    return bool;
+}
+
+
 // for (var i = 0; i < bombsArray.length; i++) {
 //     document.getElementsByClassName('squares')[bombsArray[i]].innerHTML += '<div class="bombs"></div>';
 // }
@@ -63,29 +82,27 @@ console.log(bombsArray);
 
 var userNums = [];
 var bool1 = 'vinto';
-for (var i = 0; i < last - bombs; i++) {
-    userNums[i] = prompt('inserire numero n. ' + (i + 1) + ' ' + 'di ' + (last - bombs));
-    if (bombsArray.indexOf(parseInt(userNums[i])) != -1 && parseInt(userNums[i]) == userNums[i] && userNums[i] >= first && userNums[i] <= last) {
-        bool1 = 'perso';
-        var userNums = []; /* in caso si volesse ricominciare */
-        var bool2 = prompt('hai ' + bool1 + ', totalizzando ' + i + ' punti. vuoi ricominciare? si/no');
-        break;
-    }
-    while ((userNums.indexOf(userNums[i]) != -1 && userNums.indexOf(userNums[i]) < i) || parseInt(userNums[i]) != userNums[i] || userNums[i] < first || userNums[i] > last) {
-        alert('valore non valido');
+var i = 0;
+while (i < last - bombs && bool1 == 'vinto') {
+    do {
         userNums[i] = prompt('inserire numero n. ' + (i + 1) + ' ' + 'di ' + (last - bombs));
-        if (bombsArray.indexOf(parseInt(userNums[i])) != -1) {
+        if (isInArray(parseInt(userNums[i]),bombsArray)) {
             bool1 = 'perso';
             var userNums = []; /* in caso si volesse ricominciare */
             var bool2 = prompt('hai ' + bool1 + ', totalizzando ' + i + ' punti. vuoi ricominciare? si/no');
-            i = last - bombs; /* per bloccare for */
-            break;
         }
-    }
+        else if ((isInArray(parseInt(userNums[i]),userNums) && userNums.indexOf(userNums[i]) < i) || parseInt(userNums[i]) != userNums[i] || userNums[i] < first || userNums[i] > last) { /* è stato già inserito o è un carattere o valore non valido */
+            alert('valore non valido');
+        }
+    } while (((isInArray(parseInt(userNums[i]),userNums) && userNums.indexOf(userNums[i]) < i) || parseInt(userNums[i]) != userNums[i] || userNums[i] < first || userNums[i] > last) && bool1 == 'vinto')
+    i++;
 }
 
 if (bool2 == 'si') {
     location.reload();
+}
+else if (bool2 != 'no' && bool2 != 'si') {
+    alert('non ho capito ma ok');
 }
 
 if (bool1 == 'vinto') {
